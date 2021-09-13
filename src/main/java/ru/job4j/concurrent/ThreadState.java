9c145b7bd10efaf6d9933a12f6664ef2227cf046
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-09-13 20:23:14 +0300

    task 229175

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,14 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        first.start();
+        second.start();
+        while (first.isAlive() || second.isAlive()) {
+            Thread.sleep(500);
+        }
+        System.out.println("Threads are terminated");
+    }
+}
