ae0f3ab9ebe62f36648ff62abfb4c333f7aa3e83
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-11-07 19:31:21 +0300

    task 1099

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,37 @@
+package ru.job4j.pool;
+
+import java.util.LinkedList;
+import java.util.List;
+import ru.job4j.wait.SimpleBlockingQueue;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+
+    public ThreadPool() {
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(
+                    () -> {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                tasks.poll().run();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+            );
+            threads.add(thread);
+            thread.start();
+        }
+    }
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        threads.forEach(x -> x.interrupt());
+    }
+}
