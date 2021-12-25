151d5cc0048dba829cde1a4c44cff1eb7a45bd09
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-12-26 01:00:12 +0300

    task 1099 added main

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -27,11 +27,28 @@ public class ThreadPool {
         }
     }
 
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool pool = new ThreadPool();
+        Runnable job1 = () -> {
+            try {
+                Thread.sleep(10000);
+                System.out.println("job1");
+            } catch (InterruptedException e) {
+                System.out.println("job1 was interrupted");
+                Thread.currentThread().interrupt();
+            }
+        };
+        Runnable job2 = () -> System.out.println("job2");
+        pool.work(job1);
+        pool.work(job2);
+        pool.shutdown();
+    }
+
     public void work(Runnable job) throws InterruptedException {
         tasks.offer(job);
     }
 
     public void shutdown() {
-        threads.forEach(x -> x.interrupt());
+        threads.forEach(Thread::interrupt);
     }
 }
