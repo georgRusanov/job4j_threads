57147bccf4c35108a375ccf609a36aaff24ab3c9
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-12-26 18:01:01 +0300

    task 361626 review

--- src/main/java/ru/job4j/executorService/EmailNotification.java
+++ src/main/java/ru/job4j/executorService/EmailNotification.java
@@ -5,23 +5,30 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    static ExecutorService pool = Executors.newFixedThreadPool(
+    private final static ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors()
     );
 
-    public static void emailTo(User user) {
+    public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.username, user.email);
         String body = String.format("Add a new event to %s", user.username);
         send(subject, body, user.email);
     }
 
-    public static void close() {
+    public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void send(String subject, String body, String email) {}
+    public void send(String subject, String body, String email) {}
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         pool.submit(() -> {
             User user = new User("user", "email");
             emailTo(user);
